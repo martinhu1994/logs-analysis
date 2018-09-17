168f14f7bc80905c2c96866479a33b3b6bb97986
Hanxiao Hu <martinhu1994@gmail.com>
2018-09-17 17:59:48 -0400

    update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,53 +1,73 @@
+#!/usr/bin/env python
+
 import psycopg2
 import calendar
 
 
 def report_one(cursor):
-    cursor.execute("\
-        SELECT articles.title, articleviews.views \
-        FROM articles JOIN articleviews ON articles.slug = articleviews.slug \
-        ORDER BY articleviews.views DESC \
-        LIMIT 3")
+    ''' Find the most popular three articles of all time. '''
+
+    try:
+        cursor.execute('''
+            SELECT articles.title, articleviews.views
+            FROM articles JOIN articleviews ON articles.slug = articleviews.slug
+            ORDER BY articleviews.views DESC
+            LIMIT 3''')
+    except psycopg2.Error as e:
+        print(e.pgerror)
+
     result = cursor.fetchall()
-    for entry in result:
-        print("\"%s\" -- %d views" % (entry[0], entry[1]))
+    for title, view in result:
+        print("\"%s\" -- %d views" % (title, view))
 
 
 def report_two(cursor):
-    cursor.execute("\
-        SELECT final.name AS name, sum(final.views) AS views\
-        FROM(\
-            (articles JOIN articleviews \
-            ON articles.slug = articleviews.slug) AS sub \
-        JOIN authors \
-        ON sub.author = authors.id) AS final \
-        GROUP BY name \
-        ORDER BY views DESC")
+    ''' List authors by the popularity of their articles. '''
+
+    try:
+        cursor.execute('''
+            SELECT final.name AS name, sum(final.views) AS views
+            FROM(
+                (articles JOIN articleviews
+                ON articles.slug = articleviews.slug) AS sub
+            JOIN authors
+            ON sub.author = authors.id) AS final
+            GROUP BY name
+            ORDER BY views DESC''')
+    except psycopg2.Error as e:
+        print(e.pgerror)
+
     result = cursor.fetchall()
-    for entry in result:
-        print("%s -- %d views" % (entry[0], entry[1]))
+    for author, view in result:
+        print("%s -- %d views" % (author, view))
 
 
 def report_three(cursor):
-    cursor.execute("\
-        SELECT final.date1, (final.error * 1.0) / (final.total * 1.0) \
-        FROM( \
-            (SELECT date_trunc('day', time) AS date1, count(*) AS total \
-            FROM log \
-            GROUP BY date1) sub1 \
-        JOIN \
-            (SELECT date_trunc('day', time) AS date2, count(*) AS error \
-            FROM log \
-            WHERE status != '200 OK' \
-            GROUP BY date2) sub2 \
-        ON sub1.date1 = sub2.date2) final \
-        WHERE ((final.error * 1.0) / (final.total * 1.0)) > 0.01")
+    ''' Find the days that more than 1% of requests lead to errors. '''
+
+    try:
+        cursor.execute('''
+            SELECT final.date1, (final.error * 1.0) / (final.total * 1.0)
+            FROM(
+                (SELECT date_trunc('day', time) AS date1, count(*) AS total
+                FROM log
+                GROUP BY date1) sub1
+            JOIN
+                (SELECT date_trunc('day', time) AS date2, count(*) AS error
+                FROM log
+                WHERE status != '200 OK'
+                GROUP BY date2) sub2
+            ON sub1.date1 = sub2.date2) final
+            WHERE ((final.error * 1.0) / (final.total * 1.0)) > 0.01''')
+    except psycopg2.Error as e:
+        print(e.pgerror)
+
     result = cursor.fetchall()
-    for entry in result:
-        month = calendar.month_name[entry[0].month]
-        day = entry[0].day
-        year = entry[0].year
-        percent = entry[1] * 100
+    for date, ratio in result:
+        month = calendar.month_name[date.month]
+        day = date.day
+        year = date.year
+        percent = ratio * 100
         print("%s %d, %d -- %.1f%% errors" % (month, day, year, percent))
 
 
